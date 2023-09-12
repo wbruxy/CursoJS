f3f9153f163145cbcc0668ed30b4d4c32aae1c4c
wbruxy <brunaa0407@gmail.com>
2023-09-12 17:26:48 -0300

    "console.log" e Exercício

---
+++ aula1/index.js
@@ -0,0 +1,6 @@
+console.log('Bruna'); // String = Texto
+console.log('"Bruna"'); // String = Texto
+
+console.log(123456, 15.454); // Number = Número
+
+console.log('Meu nome é "Bruna". Estou aprendendo JavaScript às', 17, 'da tarde.'); // Exercício
