e5df199135b84eeb224d9877b86cf85ac4a18bbe
wbruxy <brunaa0407@gmail.com>
2023-09-13 19:01:33 -0300

    comentários e variáveis

---
+++ aula4/comentarios.js
@@ -0,0 +1,7 @@
+// Comentário
+console.log('Olá mundo!'); // Outro comentário
+
+/* 
+Um bloco de código
+Várias linhas de comentário.
+*/
---
+++ aula6/index.js
@@ -0,0 +1,32 @@
+// Variáveis
+
+/*let nome = 'João'; // String
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com', nome, 'em 2015.');
+console.log('O filho de', nome, 'se chama Eduardo.');*/
+
+/*let nome; // Declarou a variável
+nome = 'Bruna'; // Inicializando a variável
+console.log(nome);
+nome = 'Bruxy';
+console.log(nome);*/
+
+// Não podemos criar variáveis com palavras reservadas, "let if", "let console", "let let"...
+// Variáveis precisam ter nomes significativos
+let nomeClinte = 'Luan';
+console.log(nomeClinte);
+
+// Não pode começar o nome de uma variável com um número: let no1me;
+// Não podem conter espaços ou traços: let nome Cliente
+// Utilizamos camelCase
+// Case-sensitive
+// Não podemos redeclarar variáveis com let
+// NÃO UTILLIZE "var", UTILIZE "let".
+let nomeCompletoDoCliente = 'Bruna Silva';
+console.log(nomeCompletoDoCliente);
+
+let nomecliente = 'Bruxy';
+console.log(nomeClinte, nomecliente);
